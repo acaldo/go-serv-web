42c4b8587ccbbc7d010817e744b5492db91a1044
caldev <caldart123@gmail.com>
2022-05-23 22:39:33 -0300

    catch error's

--- main.go
+++ main.go
@@ -13,10 +13,16 @@ func main() {
 	scanner.Scan()
 	operacion := scanner.Text()
 	fmt.Println(operacion)
-	valores := strings.Split(operacion, "+")
+	operador := "-"
+	valores := strings.Split(operacion, operador)
 	fmt.Println(valores)
 	fmt.Println(valores[0] + valores[1])
-	operador1, _ := strconv.Atoi(valores[0])
-	operador2, _ := strconv.Atoi(valores[1])
-	fmt.Println(operador1 + operador2)
+	operador1, err1 := strconv.Atoi(valores[0])
+	operador2, err2 := strconv.Atoi(valores[1])
+	if err1 != nil || err2 != nil {
+		fmt.Println("Error")
+	} else {
+		fmt.Println(operador1 + operador2)
+	}
+
 }
