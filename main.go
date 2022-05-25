41c6c81010c9a3f6b4e258d751b47f5025dfe89d
caldev <caldart123@gmail.com>
2022-05-25 00:20:55 -0300

    slices takslist

--- main.go
+++ main.go
@@ -8,6 +8,16 @@ type task struct {
 	done        bool
 }
 
+var taskList = []task{}
+
+func addTask(task task) {
+	taskList = append(taskList, task)
+}
+
+func deleteTask(index int) {
+	taskList = append(taskList[:index], taskList[index+1:]...)
+}
+
 func (t *task) doneTask() {
 	t.done = true
 }
@@ -26,9 +36,22 @@ func main() {
 		description: "This is task 1",
 		/*done:        false,*/ //es redundante ya que el ZERO VALUE de un BOOLEAN es FALSE
 	}
-	fmt.Printf("%+v\n", t)
+	t1 := task{
+		name:        "Task 2",
+		description: "This is task 2",
+		/*done:        false,*/ //es redundante ya que el ZERO VALUE de un BOOLEAN es FALSE
+	}
+	//fmt.Printf("%+v\n", t)
 	t.doneTask()
 	t.updateName("finish")
 	t.updateDescription("finish task")
-	fmt.Printf("%+v\n", t)
+	//fmt.Printf("%+v\n", t)
+	fmt.Println(taskList)
+	addTask(t)
+	fmt.Printf("%+v\n", taskList[0])
+	addTask(t1)
+	fmt.Printf("%+v\n", taskList)
+	deleteTask(0)
+	fmt.Printf("%+v\n", taskList)
+
 }
