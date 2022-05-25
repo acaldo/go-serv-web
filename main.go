f4cec4d1030dc9167c73abd534b7a687ea043f27
caldev <caldart123@gmail.com>
2022-05-25 01:08:47 -0300

    iteration factorization

--- main.go
+++ main.go
@@ -18,8 +18,22 @@ func deleteTask(index int) {
 	taskList = append(taskList[:index], taskList[index+1:]...)
 }
 
+func iterateTask(list []task) {
+	finish := []task{}
+	dont := []task{}
+	for _, task := range list {
+		if task.done == true {
+			finish = append(finish, task)
+		} else if task.done == false {
+			dont = append(dont, task)
+		}
+	}
+	fmt.Printf("Finish: %+v\n", finish)
+	fmt.Printf("Dont: %+v\n", dont)
+}
+
 func (t *task) doneTask() {
-	t.done = true
+	t.done = !t.done
 }
 
 func (t *task) updateDescription(newDescription string) {
@@ -34,28 +48,35 @@ func main() {
 	t := task{
 		name:        "Task 1",
 		description: "This is task 1",
-		/*done:        false,*/ //es redundante ya que el ZERO VALUE de un BOOLEAN es FALSE
+		done:        false, //es redundante ya que el ZERO VALUE de un BOOLEAN es FALSE
 	}
 	t1 := task{
 		name:        "Task 2",
 		description: "This is task 2",
-		/*done:        false,*/ //es redundante ya que el ZERO VALUE de un BOOLEAN es FALSE
+		done:        true,
+	}
+	t2 := task{
+		name:        "Task 2",
+		description: "This is task 2",
+		done:        false,
 	}
 	//fmt.Printf("%+v\n", t)
-	t.doneTask()
+	//t.doneTask()
 	t.updateName("finish")
 	t.updateDescription("finish task")
 	//fmt.Printf("%+v\n", t)
 	fmt.Println(taskList)
 	addTask(t)
 	addTask(t1)
+	addTask(t2)
 	//for
-	for i := 0; i < len(taskList); i++ {
-		fmt.Printf("%d: %+v\n", i, taskList[i])
-	}
-	//for range
-	for i, task := range taskList {
-		fmt.Printf("%d: %+v\n", i, task)
-	}
+	/* 	for i := 0; i < len(taskList); i++ {
+	   		fmt.Printf("%d: %+v\n", i, taskList[i])
+	   	}
+	   	//for range
+	   	for i, task := range taskList {
+	   		fmt.Printf("%d: %+v\n", i, task)
+	   	} */
+	iterateTask(taskList)
 
 }
